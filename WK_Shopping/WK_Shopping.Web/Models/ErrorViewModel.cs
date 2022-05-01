f96812fdf400fa632d29fa6e48fd882a3b1794b4
Adriano Araújo <tigerland1055@gmail.com>
2022-05-01 09:15:38 -0300

    Criação dos Projetos: Cliente Web, Produto API e Categoria API

---
+++ WK_Shopping/WK_Shopping.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WK_Shopping.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
